9b7009de5ff04ee137d19a2d25effe01de90aef0
cheynskjento <cheynskjent@gmail.com>
2026-01-10 15:46:27 +0100

    define typed navigation

---
+++ src/types/navigation.ts
@@ -0,0 +1,10 @@
+export type HomeStackParamList = {
+  ProductList: undefined;
+  ProductDetail: { id: number };
+};
+
+export type RootTabParamList = {
+  Home: undefined;
+  Cart: undefined;
+  Profile: undefined;
+};
