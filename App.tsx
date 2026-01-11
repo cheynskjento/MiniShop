e74f7ecd3078ceba82b8e8dad8422311897d2a9e
cheynskjento <cheynskjent@gmail.com>
2026-01-11 10:59:21 +0100

    setup providers and restore persisted state

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+import AppNavigator from './src/app/AppNavigator';
+import { store } from './src/store/store';
+import { DarkNavTheme, LightNavTheme } from './src/theme/navigationTheme';
+import type { RootState } from './src/store/store';
+import { setCart } from './src/store/cartSlice';
+import { setTheme } from './src/store/themeSlice';
+
+const queryClient = new QueryClient();
+
+function AppWithTheme() {
+  const dispatch = useDispatch();
+  const dark = useSelector((state: RootState) => state.theme.dark);
+
+  useEffect(() => {
+    (async () => {
+      const cart = await AsyncStorage.getItem('cart');
+      const theme = await AsyncStorage.getItem('theme');
+
+      if (cart) dispatch(setCart(JSON.parse(cart)));
+      if (theme) dispatch(setTheme(JSON.parse(theme).dark));
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={dark ? 'light-content' : 'dark-content'} />
+      <NavigationContainer theme={dark ? DarkNavTheme : LightNavTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AppWithTheme />
+      </QueryClientProvider>
+    </Provider>
+  );
+}
